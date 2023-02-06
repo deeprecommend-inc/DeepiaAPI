3f3782bd64d96f03133e21b57457c57f07b47986
root <root@DESKTOP-MGOMRR6.localdomain>
2023-02-07 08:26:07 +0900

    log -> content

--- src/category/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.db.models import fields
-from rest_framework import serializers
-from category.models import Category
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
--- src/category_log/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CategoryLogConfig(AppConfig):
-    name = 'category_log'
