5aa2d1db1573c181587493d5aef6a0a881f6700f
root <root@DESKTOP-MGOMRR6.localdomain>
2023-06-19 20:56:10 +0900

    user created_at

--- src/user/models.py
+++ src/user/models.py
@@ -10,9 +10,13 @@ class User(models.Model):
     image = models.TextField(blank=True, null=True)
     email = models.EmailField(unique=True, blank=False, null=False)
     password = models.CharField(max_length=128, blank=False, null=False, default='')
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.name
+    
+    class Meta:
+       ordering = ["-created_at"]
 
 
 class UserFollowing(models.Model):
